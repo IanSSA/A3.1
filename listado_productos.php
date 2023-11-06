1e732968ad9eb1878e618b8a0be96a338ae4268b
Ian Santana <iansantiagosantanaangulo@alumno.ieselrincon.es>
2023-11-06 12:08:00 +0000

    dia4

--- listado_productos.php
+++ listado_productos.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        table {margin: 0 auto;
+            text-align: center; 
+            border-color: black;
+        }
+    </style>
     <?php
         $servername = "localhost";
         $username = "mitiendaonline";
@@ -13,26 +19,44 @@
 
         if ($conn->connect_error) {
           die("Connection failed: " . $conn->connect_error);
+        }
         echo "Connected successfully";
 
         $query = "SELECT * FROM productos";
         echo "<b> <center>Database Output</center> </b> <br> <br>";
 
         if ($result = $conn->query($query)) {
-
+            echo "<div>";
+            echo '<table border="1">';
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Nombre</th>";
+            echo "<th>Precio</th>";
+            echo "<th>Imagen</th>";
+            echo "<th>Categoría</th>";
+            echo "</tr>";
             while ($row = $result->fetch_assoc()) {
-                $field1name = $row["id"];
-                $field2name = $row["Nombre"];
-                $field3name = $row["Precio"];
-                $field4name = $row["Imagen"];
-                $field5name = $row["Categoría"];
+                $id = $row["id"];
+                $Nombre = $row["Nombre"];
+                $Precio = $row["Precio"];
+                $Imagen = $row["Imagen"];
+                $idcategoria = $row["Categoría"];
+                $categoria = "SELECT Nombre FROM categorías WHERE Id=$idcategoria";
+                $categoria = $conn->query($categoria);
+                $categoria = $categoria->fetch_assoc();
+                $categoria = $categoria["Nombre"];
                 echo "<br>";
-                echo '<b>'.$field1name.'</b><br />';
-                echo $field2name.'<br />';
-                echo $field3name.'<br />';
-                echo $field4name.'<br />';
-                echo $field5name. "<br>";
+                echo "<tr>";
+                echo '<td>'.$id.'</td>';
+                echo '<td>'.$Nombre.'</td>';
+                echo '<td>'.$Precio.'</td>';
+                echo '<td><img src="imagenes/'.$Imagen.'"width="80" height="80"/></td>';
+                echo '<td>'.$categoria.'</td>';
+                echo "</tr>";
+                echo "<br>";
             }
+            echo '</table>';
+            echo "</div>";
         }
 
         /*freeresultset*/
