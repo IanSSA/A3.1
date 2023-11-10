b2d4b4f1c560f6e7818a4a3ba839b61214645aa2
PedroElRincón <pedrogarciasantana@alumno.ieselrincon.es>
2023-11-10 12:03:14 +0100

    cambios acabados

--- elimina_producto.php
+++ elimina_producto.php
@@ -7,66 +7,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="edita_producto.css">
         <title>Eliminar producto</title>
-
         <?php
-            $servername = "localhost";
-            $username = "mitiendaonline";
-            $password = "mitiendaonline";
-            $db = "mitiendaonline";
-            session_start();
-    
-            $conn = new mysqli($servername, $username, $password, $db);
-    
-            if ($conn->connect_error) {
-              die("Connection failed: " . $conn->connect_error);
-            }
-
-            $id_cambiar = $_GET['id_eliminar'];
-            if ($id_cambiar == ""){
-                echo '<style type="text/css">
-                        body {
-                            text-align: center;
-                        }
-                        #div {
-                            display: none;
-                        }
-                        </style>';
-                $productos = "SELECT * FROM productos";
-                if ($result = $conn->query($productos)) {
-                    $server = $_SERVER['REQUEST_URI'];
-                    echo "<div>";
-                    echo "<h1>ID no encotrada, seleccione el producto a editar</h1>";
-                    echo "<form action='$server' method='get'>";
-                    echo "<select name='id_editar'>";
-                    while ($row = $result->fetch_assoc()) {
-                        $id = $row["id"];
-                        $Nombre = $row["Nombre"];
-                        echo '<option value='.$id.'>'.$Nombre.'</option>';
-                        echo "<br>";
-                    }
-                    echo '</select><br>';
-                    echo '<button type="submit" class="submit btn btn-primary mt-3">Enviar</button>';
-                    echo '</form>';
-                    echo "</div>";
-                }
-            } else {
-
-                    $sql = "DELETE FROM productos WHERE id='$id_cambiar'";
-
-                    if (mysqli_query($conn, $sql)) {
-                        echo "Se ha eliminado correctamente";
-                        echo '<style type="text/css">
-                        #div {
-                            display: none;
-                        }
-                        </style>';
-                    } else {
-                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-                    }
-                    mysqli_close($conn);
-                }
-        
-    ?>
+        echo "xd";
+        ?>
     </head>
     <body class="d-flex align-items-center">  
         <div name="div"  <?php if ($id_cambiar == ""){ echo 'style="display:none;"'; } ?>>
